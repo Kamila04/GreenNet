c2f7b49b6b83f6151cbe3dc198c0dbbf2f67ccdf
luis <lramirez68@ucol.mx>
2024-03-07 18:03:55 -0600

    mas css

--- view/index.php
+++ view/index.php
@@ -3,7 +3,7 @@ if(session_status() !== PHP_SESSION_ACTIVE) session_start();
 if(isset($_SESSION['name'])){
     $datos_usuario = $_SESSION['name'];
 } else {
-    $datos_usuario = '<a href="./?m=login">Login/Sing up</a>';
+    $datos_usuario = '<a class="unirsebtn" href="./?m=login">Registrate!</a>';
 }
 ?>
 
@@ -13,19 +13,19 @@ if(isset($_SESSION['name'])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GREENNET</title>
-    <link rel="stylesheet" href="css/fonts.css">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="shortcut icon" type="image/x-icon" href="img/logo_mission_vision.png">
+    <link rel="stylesheet" href="view/css/fonts.css">
+    <link rel="stylesheet" href="view/css/style.css">
+    <link rel="shortcut icon" type="image/x-icon" href="iview/mg/logo_mission_vision.png">
 </head>
 <body>
     <header>
         <div class="logo">
-            <img src="img/logo_mission_vision.png" alt="Logo de mi foro" class="img-logo">
+            <img src="view/img/logo_mission_vision.png" alt="Logo de mi foro" class="img-logo">
             <h1 class="nombre-logo">GreenNet</h1>
         </div>
         <div class="perfil">
             <p class="nombre-perfil"><?php echo $datos_usuario ?></p>
-            <img src="img/perfil.jpg" alt="Foto de perfil" class="img-perfil">
+            <img src="view/img/perfil.jpg" alt="Foto de perfil" class="img-perfil">
         </div>
     </header>
     <div class="app">
@@ -47,16 +47,20 @@ if(isset($_SESSION['name'])){
                     <li>MrBeast construye 100 pozos en África</li>
                 </ul>
             </div>
-            <footer><p>Copyright 2024© GreenNet</p></footer>
+            <span class="copyright">Copyright 2024© GreenNet</span>
         </aside>
         <main class="publicaciones">
-            <section class="mi-perfil">
-                <img src="img/perfil_img.jpg" alt="Mi imagen">
-                <div>
-                    <h3>MiPerfil</h3>
-                    <p>Crear Publicación</p>
-                </div>
-            </section>
+        <div class="mi-perfil">
+            <div class="image-container">
+                <span>Crear Post</span>
+                <img src="view/img/perfil_img.jpg" alt="Imagen">
+                <button>Enviar</button>
+            </div>
+            <div class="input-container">
+                <input type="text" placeholder="Título">
+                <textarea placeholder="Escribe tu ídea..."></textarea>
+            </div>
+        </div>
             <section class="feed">
 
             </section>
