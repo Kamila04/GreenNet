1d49fa729d42118cce148ba402b78616f44e7919
Alang315 <alguienxc2@hotmail.com>
2024-03-11 18:37:39 -0600

    leayouts footer y header

--- view/index.php
+++ view/index.php
@@ -1,36 +1,16 @@
 <?php
-try{
-if(session_status() !== PHP_SESSION_ACTIVE) session_start();
-if(isset($_SESSION['name'])){
-    $datos_usuario = $_SESSION['name'];
-    $logout = "<li><a href='". urlsite. "/logout'>Cerrar Sesión</a></li>";
-} else {
-    $datos_usuario = '<a class="unirsebtn" href="' .urlsite . '/login">Registrate!</a>';
-    $logout = "";
-}
+    if(session_status() !== PHP_SESSION_ACTIVE) session_start();
+    if(isset($_SESSION['name'])){
+        $datos_usuario = $_SESSION['name'];
+        $logout = "<li><a href='". urlsite. "/logout'>Cerrar Sesión</a></li>";
+    } else {
+        $datos_usuario = '<a class="unirsebtn" href="' .urlsite . '/login">Registrate!</a>';
+        $logout = "";
+    }     
+    include("./view/layout/main_header.php");
+    include("./view/layout/main_footer.php");
+    main_header($datos_usuario);
 ?>
-
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>GREENNET</title>
-    <link rel="stylesheet" href=<?php echo urlsite ."/view/css/fonts.css"; ?>>
-    <link rel="stylesheet" href= <?php echo urlsite ."/view/css/style.css"; ?>>
-    <link rel="shortcut icon" type="image/x-icon" href= <?php echo urlsite ."/view/img/logo_mission_vision.png"; ?> >
-</head>
-<body>
-    <header>
-        <div class="logo">
-            <img src= <?php echo urlsite ."/view/img/logo_mission_vision.png"; ?> alt="Logo de mi foro" class="img-logo">
-            <h1 class="nombre-logo">GreenNet</h1>
-        </div>
-        <div class="perfil">
-            <p class="nombre-perfil"><?php echo $datos_usuario ?></p>
-            <img src= <?php echo urlsite ."/view/img/perfil.jpg"; ?> alt="Foto de perfil" class="img-perfil" id="perfil_Icono">
-        </div>
-    </header>
     <div class="app">
     <div id="detailsDiv">
         <span>GreenNet</span>
@@ -90,10 +70,8 @@ if(isset($_SESSION['name'])){
         </section>
         
     </div>
-    <script src=<?php echo urlsite. "/view/js/fun_script.js" ?>></script>
 </body>
+<?php main_footer();?>
 </html>
-<?php } catch(Exception $e){
-    header("Location: ./error404");
-} 
+<?php 
 ?>
---
+++ view/layout/login_footer.php
@@ -0,0 +1,7 @@
+<?php function login_footer($args = null){ ?>
+    <footer>
+        <script src=<?php echo urlsite."/view/js/login_script.js";?>></script>
+
+    </footer>
+
+<?php } ?>
